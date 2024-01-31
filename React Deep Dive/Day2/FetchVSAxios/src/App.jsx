d22a85016589dc8f2d11c2c295ab05479d08555f
Sahil <Sahil2003@gmail.com>
2024-01-31 18:07:12 +0530

    Day2 : what is the diff b/w axios and fetch

---
+++ React Deep Dive/Day2/FetchVSAxios/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>What is the difference between axios and fetch ?</h2>
+
+      <div>
+        Fetch is the native browser API for making HTTP requests. Whereas axios is a third-party library that is built on top of the native fetch API.
+        <br />
+        Check this for better understanding
+        <a href="https://replit.com/@sa20128hil/fetchVSaxios#index.js">{" "}click here</a>
+
+      </div>
+    </>
+  )
+}
+
+export default App
